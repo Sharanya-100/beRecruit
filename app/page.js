784e6d7b94ea3cfbd5cf858b67198f649ab7eb5a
ranadebsaha <ranadebsaha2002@gmail.com>
2025-08-17 20:31:04 +0530

    (main) folder updated

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Dashboard from "./dashboard/page";
+import DashboardProvider from "./provider";
+
 
 export default function Home() {
   return (
     <div>
-      Hello 
+      Hello <DashboardProvider/>
     </div>
   );
 }
